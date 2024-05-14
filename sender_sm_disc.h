b6432db7039ec90a31abae4aa86eaa75d7df022d
Maria-Miguel <166872793+Maria-Miguel@users.noreply.github.com>
2024-05-14 17:44:44 +0100

    Add files via upload
    Código do sender com envio do disc, leitura do disc do receiver e envio do ua final

---
+++ sender_sm_disc.h
@@ -0,0 +1,124 @@
+#include <stdbool.h>
+
+#define FLAG    0x5c
+#define SET     0x07
+#define UA      0x06
+#define DISC    0x0a
+#define I0      0x80
+#define I1      0xc0
+#define RR0     0x01
+#define RR1     0x11
+#define REJ0    0x05
+#define REJ1    0x15
+#define ADDR_TRANSMITTER 0x01
+#define ADDR_RECEIVER    0x03
+#define FALSE 0
+#define TRUE 1
+
+
+typedef enum {
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    SM_STOP
+} State;
+
+typedef enum {
+    UA_frame,
+    DISC_frame,
+    RR_REJ_frame,
+} ExpectedFrame;
+
+
+/*
+*  @brief State machine structure.
+    *  @param state: current state of the state machine.
+    *  @param expected_frame: expected frame to be recieved.
+    *  @param last_I_flag: last I flag recieved.
+    *  @param last_byte: last byte recieved.
+    *  @param bcc2: bcc of the data frame.
+    *  @param bytes_downloaded: number of information bytes downloaded.
+*/
+typedef struct {
+    State state;
+    ExpectedFrame expected_frame;
+    unsigned char expected_RR_flag;
+    unsigned char expected_REJ_flag;
+    bool packet_rejected;
+} StateMachine;
+
+
+/*
+*  @brief Creates a new state machine and initializes it.  
+    *  @return A new state machine.  
+*/
+StateMachine NewStateMachine();
+
+
+/*
+ *  @brief Updates the state of the state machine according to the recieved byte. 
+    *  @param  byte: recieved byte
+    *  @param  *sm: reference to the state machine
+    *  @param  DEBUG: flag to print debug messages
+    *  @return -2 if a wrong bcc is recieved.
+    *          -1 if the recieved byte is garbage.
+    *          0 if the state machine updated its state successfuly.
+    *          1 if the state machine is ready to send UA.           
+ */
+int UpdateState(unsigned char byte, StateMachine* sm, bool DEBUG);
+
+
+/*
+ *  @brief Starts the communication to reciever sending a SET frame. 
+    *  @param  fd: file descriptor of the serial port
+    *  @param  *sm: reference to the state machine
+    *  @param  DEBUG: flag to print debug messages
+    *  @return -1 if the state machine could not be started.
+    *          0 if the state machine was started successfuly.
+ */
+int Set(int fd, StateMachine* sm, bool DEBUG);
+
+
+
+/*
+ *  @brief Sends a response according to the state of the state machine. 
+    *  @param  fd: file descriptor of the serial port
+    *  @param  *sm: reference to the state machine
+    *  @param  control_flag: control flag of the response
+    *  @param  DEBUG: flag to print debug messages
+    *  @return -1 if the response could not be sent.
+    *          0 if the response was sent successfuly.
+ */
+int SendResponse(int fd, StateMachine *sm, unsigned char control_flag, bool DEBUG);
+
+/*
+ *  @brief Ends the communication to reciever sending a DISC frame. 
+    *  @param  fd: file descriptor of the serial port
+    *  @param  DEBUG: flag to print debug messages
+    *  @return -1 if the request for termination was not sent.
+    *          0 if the request was sent successfuly.
+ */
+int Send_Termination(int fd, bool DEBUG);
+
+/*
+ *  @brief Updates the state of the disc state machine according to the recieved byte. 
+    *  @param  byte: recieved byte
+    *  @param  *sm: reference to the state machine
+    *  @param  DEBUG: flag to print debug messages
+    *  @return -2 if a wrong bcc is recieved.
+    *          -1 if the recieved byte is garbage.
+    *          0 if the state machine updated its state successfuly.
+    *          1 if the state machine is ready to send UA.           
+ */
+int ShortUpdateState(unsigned char byte, StateMachine* sm, bool DEBUG);
+
+/*
+ *  @brief Ends the communication to reciever sending a UA frame. 
+    *  @param  fd: file descriptor of the serial port
+    *  @param  DEBUG: flag to print debug messages
+    *  @return -1 if the state machine could not be started.
+    *          0 if the state machine was started successfuly.
+ */
+int Ua(int fd, bool DEBUG);
---
+++ writenoncanonical_disc.c
@@ -0,0 +1,155 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "sender_sm.h"
+#include <stdbool.h>
+#include "byte_stuff.h"
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+#define DEBUG true
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
+    int i, sum = 0, speed = 0;
+
+    if ( (argc < 3) ||
+         ((strcmp("/dev/ttyS10", argv[1])!=0) &&
+          (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+        exit(1);
+    }
+
+
+    /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+    */
+
+
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd < 0) { perror(argv[1]); exit(-1); }
+
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+
+    /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) próximo(s) caracter(es)
+    */
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+    memset(buf,0,255);
+   
+
+    StateMachine sm = NewStateMachine();
+    if (Set(fd, &sm, DEBUG) == -1){
+        perror("Set"); //usar timer aqui?
+        exit(-1);
+    }
+    sleep(1);
+
+    //pegar o input do usuario:
+    char *input = (char*)malloc(strlen(argv[2])+1);
+    strcpy(input, argv[2]);
+    printf("Input: %s\n", input);
+
+    unsigned char bcc2;
+    for (int i = 0; i < strlen(input); i++){
+        bcc2 ^= input[i];
+    }
+    byte_stuff(&input);
+    printf("Input after byte stuffing: %s\n", input);
+    byte_destuff(&input);
+    printf("Input after byte destuffing: %s\n", input);
+    buf[0] = FLAG;
+    buf[1] = ADDR_TRANSMITTER;
+    buf[2] = I0;
+    buf[3] = ADDR_TRANSMITTER^buf[2];
+    buf[4] = FLAG;
+    for (int i = 0; i < strlen(input); i++){
+        buf[i+5] = input[i];
+    }
+
+    buf[strlen(input)+5] = bcc2;
+    buf[strlen(input)+6] = FLAG;
+
+    while(read(fd,buf,1) == 1){
+        printf("Reading\n");
+        if (UpdateState(buf[0], &sm, DEBUG) == 1){
+            write(fd, buf, strlen(input)+7);
+            break;
+        }
+    }
+
+    
+    StateMachine smf = DiscStateMachine(); // cria uma state machine para receber disc do recetor
+
+
+    if (Send_Termination(fd, DEBUG) == -1){ // envia o disc ao recetor
+        perror("Disc"); 
+        exit(-1);
+    }
+    sleep(1);
+
+    while(read(fd,buf,1) == 1){
+        printf("Reading\n");  // le a resposta do receiver e depois envia ua... , falta a state machine para receber ambos
+        if(ShortUpdateState(buf[0], &smf,DEBUG)){
+            if (Set(fd, &sm, DEBUG) == -1){
+                perror("Ua"); 
+                exit(-1);
+                }
+        }
+    }
+    
+    
+
+    sleep(1);
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+
+    close(fd);
+    return 0;
+}
