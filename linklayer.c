2b6171192b39b3fa69975ec38db2d16969c11589
Maria-Miguel <166872793+Maria-Miguel@users.noreply.github.com>
2024-05-22 16:51:29 +0100

    Update linklayer.c

--- linklayer.c
+++ linklayer.c
@@ -107,3 +107,59 @@ int llread(unsigned char* packet){
 
     return -1; // Falha
 }
+llclose(linkLayer connectionParameters, int showStatistics){
+    
+    if(connectionParameters->role==0){
+        StateMachine_s smf = DiscStateMachine(); // cria uma state machine para receber disc do recetor
+
+
+    if (Send_Termination(fd, DEBUG) == -1){ // envia o disc ao recetor
+        perror("Disc"); 
+        exit(-1);
+    }
+    sleep(1);
+
+    while(read(fd,buf,1) == 1){
+        printf("Reading\n");  // le a resposta do receiver e depois envia ua... , falta a state machine para receber ambos
+        if(ShortUpdateState(buf[0], &smf,DEBUG)){
+            if (Set(fd, &sm, DEBUG) == -1){
+                perror("Ua"); 
+                exit(-1);
+                }
+        }
+    }
+    sleep(1);
+    }
+
+    else if(connectionParameters->role==1){ //colocar disc receiver
+         
+        STOP=FALSE;
+        StateMachine sm = NewStateMachine();
+        sm->expected_frame=DISC_frame;
+        while (STOP==FALSE) {       /* loop for input */
+        res = read(fd,buf,1);   /* returns after 5 chars have been input */  
+        if (res == -1){
+            perror("read");
+            exit(-1);
+        }
+        int sm_res;
+        sm_res = UpdateState(buf[0], &sm, fd, DEBUG);
+
+    }
+    }
+
+    else return -1;
+
+    if(showStatistics==TRUE){
+        printf("%d\n",connectionParameters->numTries);// estatistica?
+    }
+
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 1;
+}
